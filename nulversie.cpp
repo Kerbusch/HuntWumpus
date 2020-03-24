137a1e61a8af86bb2fb6fe69922ed9c9fb5110a1
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2020-03-24 10:56:30 +0100

    Update nulversie.cpp

--- nulversie.cpp
+++ nulversie.cpp
@@ -9,11 +9,11 @@ vector<vector<int>> kamers = {{2,4,8},{1,3,7},{2,4,6},{1,3,5},{4,6,8},{3,5,7},{2
 int main(){
     while(true){
         int invoer;
-        cout << "Locatie: " << locatie << ". De andere locaties zijn: ";
+        cout << "Je bent in kamer: " << locatie << ". Je kan je verplaatsen naar deze kamers: ";
         for(int i = 0; i < 3; i ++){
             cout << kamers[locatie-1][i] << ", ";
         }
-        cout << "\nWaar: ";
+        cout << "\nWaar wil je heen: ";
         cin >> invoer;
         for(int i = 0; i < 3; i ++){
             if(kamers[locatie-1][i] == invoer){
