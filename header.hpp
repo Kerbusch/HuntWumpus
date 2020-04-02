0c9990c794291e86986b4a596f1dcf917481dcef
RoelStierum <RoelStierum@users.noreply.github.com>
2020-04-02 10:30:46 +0200

    Update header.hpp

--- header.hpp
+++ header.hpp
@@ -170,6 +170,22 @@ bool hoorBat(){
     }
     return false;
 }
+
+bool voelWind(){
+    // deze funtie kijkt of de valkuil binnen 1 kamers zit en als dat zo is return hij true
+    for(unsigned int i = 0; i < kamers[valkuil1-1].size(); i++){
+        if(kamers[valkuil1-1][i] == locatie){
+            return true;
+        }
+    }
+    for(unsigned int i = 0; i < kamers[valkuil2-1].size(); i++){
+        if(kamers[valkuil2-1][i] == locatie){
+            return true;
+        }
+    }
+    return false;
+}
+
 bool valkuil_check(){
     // deze functie kijkt of de speler in een valkuil terecht is gekomen.
     if(locatie == valkuil1 || locatie == valkuil2){
@@ -216,6 +232,9 @@ void driver(){
     }
     if(hoorBat()){ //kijkt of je bat hoort
         cout << "Je hoort geflapper van een Supervleermuis.\n";
+    }
+    if(voelWind()){ //kijkt of je wind voelt
+        cout << "Je voelt een lichte bries.\n";
     }
     cout << "\nJe bent in kamer: " << locatie << ". De tunnels leiden naar kamers: ";    //cout locatie
     for(int i = 0; i < 3; i ++){    //For-loop die itereerd over de vector met kamers
